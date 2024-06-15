a5ce60d6d2a4468225ccd4d0658d7312b79c4bd7
KimAYoung <dalmadbb@gmail.com>
2024-06-15 12:18:10 +0900

    fix

--- scripts/seedFriend.js
+++ scripts/seedFriend.js
@@ -9,12 +9,12 @@ const seedFriends = async () => {
     const user = await User.create({ email: 'user@example.com', password: 'password' });
 
     await Friend.bulkCreate([
-      { name: 'Friend 1', iconUrl: '/images/friend_1.png', userId: user.id },
-      { name: 'Friend 2', iconUrl: '/images/friend_2.png', userId: user.id },
-      { name: 'Friend 3', iconUrl: '/images/friend_3.png', userId: user.id },
-      { name: 'Friend 4', iconUrl: '/images/friend_4.png', userId: user.id },
-      { name: 'Friend 5', iconUrl: '/images/friend_5.png', userId: user.id },
-      { name: 'Friend 6', iconUrl: '/images/friend_6.png', userId: user.id },
+      { name: '서현이', iconUrl: '/images/friend_1.png', userId: user.id },
+      { name: '소연이', iconUrl: '/images/friend_2.png', userId: user.id },
+      { name: '인영이', iconUrl: '/images/friend_3.png', userId: user.id },
+      { name: '지현이', iconUrl: '/images/friend_4.png', userId: user.id },
+      { name: '혜원이', iconUrl: '/images/friend_5.png', userId: user.id },
+      { name: '성만이', iconUrl: '/images/friend_6.png', userId: user.id }
     ]);
 
     console.log("Test data created successfully");
