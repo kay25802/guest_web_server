248d2097e8c8dbae0fda2200a205ab35acf3bed9
KimAYoung <dalmadbb@gmail.com>
2024-06-15 18:12:31 +0900

    style | 수정

--- scripts/seedFriend.js
+++ scripts/seedFriend.js
@@ -13,12 +13,17 @@ const seedFriends = async () => {
       { name: '소연이', iconUrl: '/images/friend_2.png', userId: user.id },
       { name: '인영이', iconUrl: '/images/friend_3.png', userId: user.id },
       { name: '지현이', iconUrl: '/images/friend_4.png', userId: user.id },
-      { name: '혜원이', iconUrl: '/images/friend_5.png', userId: user.id },
-      { name: '성만이', iconUrl: '/images/friend_6.png', userId: user.id }
+      { name: '지훈이', iconUrl: '/images/friend_5.png', userId: user.id },
+      { name: '채원이', iconUrl: '/images/friend_6.png', userId: user.id }
     ]);
 
     // 친구를 위한 방명록 예시
-    await Guestbook.create({ title: 'Hello', content: 'Welcome to my guestbook!', category: 'Free', userId: user.id, friendId: friends[0].id });
+    await Guestbook.create({ title: '안녕 서현아!', content: '서현아 오늘 학교에서 너 급식 먹는 거 봤어!!', category: 'Free', userId: user.id, friendId: friends[0].id });
+    await Guestbook.create({ title: '소연아 반가워:)', content: '소연아 우리 더 친하게 지내자ㅎㅎ', category: 'Free', userId: user.id, friendId: friends[1].id });
+    await Guestbook.create({ title: '인영언니 내일 만나!', content: '우리 내일 행궁동에서 만나! 사케동도 먹구, 종강했으니깐 맥주도 마시장', category: 'Free', userId: user.id, friendId: friends[2].id });
+    await Guestbook.create({ title: '지현아 잘 지내고 있지?', content: '지현아 요즘 뭐하구 지내?? 요즘 날씨도 좋던데 종종 하늘도 올려다보며 늘 소소한 행복이 가득하길 바래!!', category: 'Free', userId: user.id, friendId: friends[3].id });
+    await Guestbook.create({ title: '지훈아 나 다녀감', content: '지훈! 나 아영인데.. 너 방명록 잘 구경하고 간다~', category: 'Free', userId: user.id, friendId: friends[4].id });
+    await Guestbook.create({ title: '채원 하이~!!', content: '채원!! 나 배고파.. 내일 우리 점심 뭐 먹지?!!! 맛있는 거 먹자~~ 설레ㅎㅎ', category: 'Free', userId: user.id, friendId: friends[5].id });
 
     console.log("Test data created successfully");
   } catch (error) {
